de5f7f7b438f0c694868c6aba87e071d7ec0fa38
Andrew Williams <andrew.th.williams@gmail.com>
2021-03-31 17:51:51 -0600

    Report code

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-// #include <mpi.h>
+#include <mpi.h>
 #include "Lab4_IO.h"
 #include "timer.h"
 
@@ -25,21 +25,23 @@
 int main(void) {
     int my_rank, comm_sz;
     struct node *nodehead;
-    int nodecount;
-    double *r, *r_pre, *contribution;
+    int nodecount, local_StartIndex, local_EndIndex;
+    int partition, padded_length;
+    double *r, *r_pre, *contribution, *local_r, *local_contribution;
     int i, j;
     double damp_const;
     int iterationcount = 0;
-    int collected_nodecount;
-    double *collected_r;
-    double cst_addapted_threshold;
+    // int collected_nodecount;
+    // double *collected_r;
+    // double cst_addapted_threshold;
     double error;
     FILE *ip;
+    double start, end;
 
     /* Init, Get rank for each process & Find total # of processes used */
-    // MPI_Init(NULL, NULL);
-    // MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    // MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
     if ((ip = fopen("data_input_meta","r")) == NULL) {
         printf("Error opening the data_input_meta file.\n");
@@ -49,48 +51,82 @@ int main(void) {
     fclose(ip);
 
     // Adjust the threshold according to the problem size
-    cst_addapted_threshold = THRESHOLD;
+    //cst_addapted_threshold = THRESHOLD;
     
     if (node_init(&nodehead, 0, nodecount)) return 254;
+    
+    // Calculate partition size for each process
+    partition = (int) nodecount/comm_sz;
+    if (partition * comm_sz != nodecount)
+        partition += 1;
+    padded_length = partition * comm_sz;
+
     // initialize variables
-    r = malloc(nodecount * sizeof(double));
-    r_pre = malloc(nodecount * sizeof(double));
+    r = malloc(padded_length * sizeof(double));
+    r_pre = malloc(padded_length * sizeof(double));
+    local_r = malloc(partition * sizeof(double));
     for ( i = 0; i < nodecount; ++i)
         r[i] = 1.0 / nodecount;
-    contribution = malloc(nodecount * sizeof(double));
+        
+    contribution = malloc(padded_length * sizeof(double));
+    local_contribution = malloc(partition * sizeof(double));
     for ( i = 0; i < nodecount; ++i)
         contribution[i] = r[i] / nodehead[i].num_out_links * DAMPING_FACTOR;
     damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
+    
     // CORE CALCULATION
-    // GET_TIME(start);
+    GET_TIME(start);
     do{
         ++iterationcount;
         vec_cp(r, r_pre, nodecount);
+        
+        /* Partition work for each process */
+        local_StartIndex = my_rank * partition;
+        local_EndIndex   = (local_StartIndex + partition) - 1;
+        
         // update the value
-        for ( i = 0; i < nodecount; ++i){
-            r[i] = 0;
+        for ( i = local_StartIndex; (i < nodecount) && (i <= local_EndIndex); ++i) {
+            local_r[i - local_StartIndex] = 0;
             for ( j = 0; j < nodehead[i].num_in_links; ++j)
-                r[i] += contribution[nodehead[i].inlinks[j]];
-            r[i] += damp_const;
+                local_r[i - local_StartIndex] += contribution[nodehead[i].inlinks[j]];
+            local_r[i - local_StartIndex] += damp_const;
         }
         // update and broadcast the contribution
-        for ( i=0; i<nodecount; ++i){
-            contribution[i] = r[i] / nodehead[i].num_out_links * DAMPING_FACTOR;
+        for ( i=local_StartIndex; (i < nodecount) && (i <= local_EndIndex); ++i){
+            local_contribution[i - local_StartIndex] = local_r[i - local_StartIndex] / nodehead[i].num_out_links * DAMPING_FACTOR;
         }
+
+        /* 'Gather' all data from each process */
+        MPI_Allgather(local_r, partition, MPI_DOUBLE, r, partition, MPI_DOUBLE, MPI_COMM_WORLD);
+        MPI_Allgather(local_contribution, partition, MPI_DOUBLE, contribution, partition, MPI_DOUBLE, MPI_COMM_WORLD);
+        
+        /* 'Gather' all data from each process into Master
+         *  MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype senddatatype, void *recvbuf, int recvcount, MPI_Datatype recvdatatype, int target, MPI_Comm comm)
+         *  MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype senddatatype, void *recvbuf, int recvcount, MPI_Datatype recvdatatype, 0, MPI_COMM_WORLD)
+         *
+         * Local send buffer // recvbuffer size = num proc * local send buffer // senddatatype = recvdatatype // target = 0 (Master) // 
+         */
     }
     while(rel_error(r, r_pre, nodecount) >= EPSILON);
-    // GET_TIME(end);
-    // printf("Program converged at %d th iteration.\nElapsed time %f.\n", iterationcount, end-start);
-
-    /* Save the data to file for Lab 4 */
-    double end_time = 10.0;
-    Lab4_saveoutput(r, nodecount, end_time);
-
-    // post processing
-    node_destroy(nodehead, nodecount);
-    free(contribution);
+    GET_TIME(end);
     
     /* Shut down MPI */
-    //MPI_Finalize();
+    MPI_Finalize();
+
+    if (my_rank == 0) {
+        printf("Program converged at %d th iteration.\nElapsed time %f.\n", iterationcount, end-start);
+
+        /* Save the data to file for Lab 4 */
+        double end_time = end - start;
+        Lab4_saveoutput(r, nodecount, end_time);
+
+        // post processing
+        node_destroy(nodehead, nodecount);
+        free(contribution);
+        free(local_contribution);
+        free(r);
+        free(r_pre);
+        free(local_r);
+    }
     return 0;
 }

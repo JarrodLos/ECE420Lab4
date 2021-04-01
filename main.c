4d6670aaa684efc0526ee5017875fe970fd04192
Jarrod Los <56139570+JarrodLos@users.noreply.github.com>
2021-03-31 20:55:00 -0600

    Updated for testing

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
  * Date:        March 31, 2021
  *
  * Input:       
- * Output:      
+ * Output:      Creates file "data_output"
  * 
- * Description: 
+ * Description: Implementation of PageRank via MPI that can run multiple processes on a single 
+ *              process or on a cluster of computers.
  * 
- * References:  
+ * References:  The algorithm in the lab manual, as well as code in serial tester and lecture examples
+ *              for initialization, creation and message passing between processes was used to supplement
+ *              our implementation of PageRank.
  */
 
 #define LAB4_EXTEND
@@ -17,6 +20,7 @@
 #include <mpi.h>
 #include "Lab4_IO.h"
 #include "timer.h"
+#include <string.h>
 
 #define EPSILON 0.00001
 #define DAMPING_FACTOR 0.85
@@ -32,10 +36,6 @@ int main(int argc, char *argv[]) {
     int i, j;
     double damp_const;
     int iterationcount = 0;
-    // int collected_nodecount;
-    // double *collected_r;
-    // double cst_addapted_threshold;
-    double error;
     FILE *ip;
     double start, end;
 
@@ -43,51 +43,74 @@ int main(int argc, char *argv[]) {
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+
+    /* Check for custom test files (Can remove for final version) 
+     * Note: Change IO file, header and node_init() to original
+     */
+    int testFile; 
     if (argc == 1) {
+        testFile = 0;
         if ((ip = fopen("data_input_meta","r")) == NULL) {
             printf("Error opening the data_input_meta file.\n");
             return 254;
         }
     } else {
-        printf("Accessing data input file: %s\n", argv[1]);
-        if ((ip = fopen(argv[1],"r")) == NULL) {
-            printf("Error opening the data_input_meta file.\n");
+        if (strcmp(argv[1], "1") == 0) {
+            testFile = 1;
+            if ((ip = fopen("1112_nodes_input_meta","r")) == NULL) {
+                printf("Error opening the 1112_nodes_input_meta file.\n");
+                return 254;
+            }
+        } else if (strcmp(argv[1], "2") == 0) {
+            testFile = 2;
+            if ((ip = fopen("5424_nodes_input_meta","r")) == NULL) {
+                printf("Error opening the 5424_nodes_input_meta file.\n");
+                return 254;
+            }
+        } else if (strcmp(argv[1], "3") == 0) {
+            testFile = 3;
+            if ((ip = fopen("10000_nodes_input_meta","r")) == NULL) {
+                printf("Error opening the 5424_nodes_input_meta file.\n");
+                return 254;
+            }
+        } else {
+            printf("Error reading custom input name.\n");
             return 254;
         }
     }
 
-    if ((ip = fopen("data_input_meta","r")) == NULL) {
-        printf("Error opening the data_input_meta file.\n");
-        return 254;
-    }
+    /* Open the meta data file created from datatrim.c & pull out the node count */
+    // if ((ip = fopen("data_input_meta","r")) == NULL) {
+    //     printf("Error opening the data_input_meta file.\n");
+    //     return 254;
+    // }
     fscanf(ip, "%d\n", &nodecount);
     fclose(ip);
-
-    // Adjust the threshold according to the problem size
-    //cst_addapted_threshold = THRESHOLD;
     
-    if (node_init(&nodehead, 0, nodecount)) return 254;
+    /* Update this to the original node_init() & fix header file as well */
+    if (node_init(&nodehead, 0, nodecount, testFile)) return 254;
+    // if (node_init(&nodehead, 0, nodecount)) return 254;
     
-    // Calculate partition size for each process
+    /* Calculate partition size for each process */
     partition = (int) nodecount/comm_sz;
     if (partition * comm_sz != nodecount)
         partition += 1;
     padded_length = partition * comm_sz;
 
-    // initialize variables
+    /* Initialize variables local to process creating the shared buffers for message passing */
     r = malloc(padded_length * sizeof(double));
     r_pre = malloc(padded_length * sizeof(double));
     local_r = malloc(partition * sizeof(double));
     for ( i = 0; i < nodecount; ++i)
         r[i] = 1.0 / nodecount;
-        
+    
     contribution = malloc(padded_length * sizeof(double));
     local_contribution = malloc(partition * sizeof(double));
     for ( i = 0; i < nodecount; ++i)
         contribution[i] = r[i] / nodehead[i].num_out_links * DAMPING_FACTOR;
     damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
     
-    // CORE CALCULATION
+    /* The core of the calculation & the main focus of our time & performance measurements */
     GET_TIME(start);
     do{
         ++iterationcount;
@@ -97,28 +120,22 @@ int main(int argc, char *argv[]) {
         local_StartIndex = my_rank * partition;
         local_EndIndex   = (local_StartIndex + partition) - 1;
         
-        // update the value
+        /* Update the value for our local r */
         for ( i = local_StartIndex; (i < nodecount) && (i <= local_EndIndex); ++i) {
             local_r[i - local_StartIndex] = 0;
             for ( j = 0; j < nodehead[i].num_in_links; ++j)
                 local_r[i - local_StartIndex] += contribution[nodehead[i].inlinks[j]];
             local_r[i - local_StartIndex] += damp_const;
         }
-        // update and broadcast the contribution
+
+        /* Update the value for our local contribution */
         for ( i=local_StartIndex; (i < nodecount) && (i <= local_EndIndex); ++i){
             local_contribution[i - local_StartIndex] = local_r[i - local_StartIndex] / nodehead[i].num_out_links * DAMPING_FACTOR;
         }
 
-        /* 'Gather' all data from each process */
+        /* 'Gather' all data from each process & share to all */
         MPI_Allgather(local_r, partition, MPI_DOUBLE, r, partition, MPI_DOUBLE, MPI_COMM_WORLD);
         MPI_Allgather(local_contribution, partition, MPI_DOUBLE, contribution, partition, MPI_DOUBLE, MPI_COMM_WORLD);
-        
-        /* 'Gather' all data from each process into Master
-         *  MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype senddatatype, void *recvbuf, int recvcount, MPI_Datatype recvdatatype, int target, MPI_Comm comm)
-         *  MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype senddatatype, void *recvbuf, int recvcount, MPI_Datatype recvdatatype, 0, MPI_COMM_WORLD)
-         *
-         * Local send buffer // recvbuffer size = num proc * local send buffer // senddatatype = recvdatatype // target = 0 (Master) // 
-         */
     }
     while(rel_error(r, r_pre, nodecount) >= EPSILON);
     GET_TIME(end);
@@ -126,20 +143,22 @@ int main(int argc, char *argv[]) {
     /* Shut down MPI */
     MPI_Finalize();
 
+    /* Have the master output the desired data */
     if (my_rank == 0) {
         printf("Program converged at %d th iteration.\nElapsed time %f.\n", iterationcount, end-start);
 
         /* Save the data to file for Lab 4 */
         double end_time = end - start;
         Lab4_saveoutput(r, nodecount, end_time);
-
-        // post processing
-        node_destroy(nodehead, nodecount);
-        free(contribution);
-        free(local_contribution);
-        free(r);
-        free(r_pre);
-        free(local_r);
     }
+
+    /* Free our initial variables */
+    node_destroy(nodehead, nodecount);
+    free(contribution);
+    free(local_contribution);
+    free(r);
+    free(r_pre);
+    free(local_r);
+
     return 0;
 }
